fd9ed93f05129e3478dc861f4112df612ed2a2d8
Tsimofey Vertsinskiy <timoxa2011gold@gmail.com>
2026-04-24 20:13:28 +0300

    Solve Task02

---
+++ Task02/main.cpp
@@ -0,0 +1,22 @@
+#include "util.h"
+
+int main() {
+	int array[DEFAULT_SIZE];
+
+	int size;
+
+	do {
+		cout << "Input size of array: ";
+		cin >> size;
+	} while (size <= 0);
+
+	init_random(array, size, -10, 20);
+
+	cout << "Array: " << convert(array, size) << endl;
+
+	int count = count_negative_elements(array, size);
+
+	cout << "Array has " << count << " negative elements.\n";
+
+	return 0;
+}
---
+++ Task02/util.h
@@ -0,0 +1,5 @@
+#include "logic.h"
+
+void init_random(int array[], int size, int a, int b);
+
+string convert(int array[], int size);
